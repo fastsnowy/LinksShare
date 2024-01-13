2e0b75763224cdb7ff3c988ca4d7aff1e8444a0e
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2024-01-13 23:24:07 +0900

    ✨ Add GitHub link to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { MyCard } from "@/components/Card"
 import { getMastodonServerList, getMisskeyServerList } from "@/libs/services"
+import { ActionIcon } from "@mantine/core"
+import { IconBrandGithub } from "@tabler/icons-react"
 
 export default async function Home() {
   const misskeyData = await getMisskeyServerList()
@@ -13,6 +15,16 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center px-4 gap-8">
       <h1 className="text-6xl">Links Share</h1>
       <MyCard misskey={misskeyInstanceList} mastodon={mastdonInstanceList} />
+      <ActionIcon
+        variant="light"
+        color="gray"
+        size="lg"
+        component="a"
+        target="_blank"
+        href="https://github.com/fastsnowy/LinksShare"
+      >
+        <IconBrandGithub />
+      </ActionIcon>
     </main>
   )
 }
