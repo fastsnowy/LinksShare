306cd4ea67d8c10322c7138cea52d00e775de583
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2025-02-11 22:29:27 +0900

    ✨ Implement API routes for Misskey and Mastodon server lists

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,36 @@
+import { zValidator } from "@hono/zod-validator"
+import { Hono } from "hono"
+import { handle } from "hono/vercel"
+import { z } from "zod"
+
+export const runtime = "edge"
+
+const app = new Hono().basePath("/api")
+
+const route = app
+  .get("/server/misskey", async (c) => {
+    const res = await fetch("https://instanceapp.misskey.page/instances.json", {
+      next: { revalidate: 60 * 60 * 24 },
+    })
+    const data = (await res.json()) as { instancesInfos: { url: string }[] }
+    const urls = data.instancesInfos.map((instance: { url: string }) => {
+      return instance.url
+    })
+    return c.json(urls)
+  })
+  .get("/server/mastodon", async (c) => {
+    const res = await fetch(
+      "https://api.joinmastodon.org/servers?language=&category=&region=&ownership=&registrations=",
+      { next: { revalidate: 60 * 60 * 24 } },
+    )
+    const data = (await res.json()) as { domain: string }[]
+    const urls = data.map((instance: { domain: string }) => {
+      return instance.domain
+    })
+    return c.json(urls)
+  })
+
+export const GET = handle(route)
+export const POST = handle(route)
+
+export type AppType = typeof route
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 import { AboutThisSite } from "@/components/About"
 import { ShareCard } from "@/components/Card"
-import { getMastodonServerList, getMisskeyServerList } from "@/libs/services"
 import { ActionIcon } from "@mantine/core"
 import { IconBrandGithub } from "@tabler/icons-react"
+import { hc } from "hono/client"
 import { Suspense } from "react"
+import { AppType } from "./api/[[...route]]/route"
+
+const hcHost =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/"
+    : "https://links-share.vercel.app/"
+console.log(hcHost)
+const client = hc<AppType>(hcHost)
 
 export default async function Home() {
-  const misskeyData = await getMisskeyServerList()
-  const mastodonData = await getMastodonServerList()
-  const mastodonInstanceList = mastodonData.map((instance) => instance.domain)
-  const misskeyInstanceList = misskeyData.data.map(
-    (instance) => instance.instance,
-  )
+  const res_mi = await client.api.server.misskey.$get()
+  const res_mstdn = await client.api.server.mastodon.$get()
+  const misskeyInstanceList = await res_mi.json()
+  const mastodonInstanceList = await res_mstdn.json()
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-4 gap-8">
