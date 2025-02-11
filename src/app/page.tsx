1af39ced8b9cd3b7cdf767171b3786ebf6224fd2
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2025-02-11 23:33:02 +0900

    🐛 Improve error handling for API requests in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,36 +10,44 @@ import { AppType } from "./api/[[...route]]/route"
 const client = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!)
 
 export default async function Home() {
-  const res_mi = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/server/misskey`,
-  )
-  const res_mstdn = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/server/mastodon`,
-  )
-  // const res_mi = await client.api.server.misskey.$get()
-  // const res_mstdn = await client.api.server.mastodon.$get()
-  const misskeyInstanceList = await res_mi.json()
-  const mastodonInstanceList = await res_mstdn.json()
+  try {
+    const res_mi = await client.api.server.misskey.$get()
+    if (!res_mi.ok) {
+      throw new Error(`Error fetching misskey instances: ${res_mi.statusText}`)
+    }
+    const res_mstdn = await client.api.server.mastodon.$get()
+    if (!res_mstdn.ok) {
+      throw new Error(
+        `Error fetching mastodon instances: ${res_mstdn.statusText}`,
+      )
+    }
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center px-4 gap-8">
-      <AboutThisSite />
-      <Suspense>
-        <ShareCard
-          misskey={misskeyInstanceList}
-          mastodon={mastodonInstanceList}
-        />
-      </Suspense>
-      <ActionIcon
-        variant="subtle"
-        color="gray"
-        size="lg"
-        component="a"
-        target="_blank"
-        href="https://github.com/fastsnowy/LinksShare"
-      >
-        <IconBrandGithub />
-      </ActionIcon>
-    </main>
-  )
+    const misskeyInstanceList = await res_mi.json()
+    const mastodonInstanceList = await res_mstdn.json()
+
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center px-4 gap-8">
+        <AboutThisSite />
+        <Suspense>
+          <ShareCard
+            misskey={misskeyInstanceList}
+            mastodon={mastodonInstanceList}
+          />
+        </Suspense>
+        <ActionIcon
+          variant="subtle"
+          color="gray"
+          size="lg"
+          component="a"
+          target="_blank"
+          href="https://github.com/fastsnowy/LinksShare"
+        >
+          <IconBrandGithub />
+        </ActionIcon>
+      </main>
+    )
+  } catch (error) {
+    console.error(error)
+    return <div>Error loading data</div>
+  }
 }
